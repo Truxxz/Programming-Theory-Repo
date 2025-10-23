3bb7f999b33c78fe049ad8d22d027a59cd5babf3
9TailedNinko <nyker.tobias@gmail.com>
2025-10-23 15:18:54 +0200

    Changed speed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private GameObject playerTarget;
-    private float speed = 10.0f;
+    private float speed = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
